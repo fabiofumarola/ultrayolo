4aa706a70dfabcee351dcedd18ae1d069e4cad34
Fabio Fumarola <fumarolaf@prometeia.com>
2020-02-03 22:42:43 +0100

    update doc

--- docs/conf.py
+++ docs/conf.py
@@ -33,10 +33,16 @@ import ultrayolo
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones. ,
 extensions = [
-    'sphinx.ext.autodoc', 
-    'sphinx.ext.viewcode', 
     'nbsphinx',
-    'sphinx.ext.githubpages']
+    "sphinx.ext.doctest",
+    "sphinx.ext.todo",
+    "sphinx.ext.coverage",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.napoleon"
+    ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
